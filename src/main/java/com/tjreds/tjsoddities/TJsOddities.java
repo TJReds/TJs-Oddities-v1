b46be82f72419a2100c482ad1e55191a65cab3c2
TJReds <tylerredding01@gmail.com>
2023-11-17 17:49:36 -0500

    -m"Added Star Shard and Star Block"

--- src/main/java/com/tjreds/tjsoddities/TJsOddities.java
+++ src/main/java/com/tjreds/tjsoddities/TJsOddities.java
@@ -5,6 +5,10 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.tjreds.tjsoddities.block.ModBlocks;
+import com.tjreds.tjsoddities.item.ModItemGroups;
+import com.tjreds.tjsoddities.item.ModItems;
+
 public class TJsOddities implements ModInitializer {
 	public static final String MOD_ID = "tjsoddities";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
@@ -12,6 +16,8 @@ public class TJsOddities implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/com/tjreds/tjsoddities/item/ModItemGroups.java
@@ -0,0 +1,32 @@
+package com.tjreds.tjsoddities.item;
+
+import com.tjreds.tjsoddities.TJsOddities;
+import com.tjreds.tjsoddities.block.ModBlocks;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+
+public class ModItemGroups {
+
+    public static final ItemGroup STAR_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(TJsOddities.MOD_ID, "star_group"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.star_group"))
+                    .icon(() -> new ItemStack(ModItems.STAR_SHARD)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.STAR_SHARD);
+                        entries.add(ModBlocks.STAR_BLOCK);
+
+
+                    }).build());
+
+
+    public static void registerItemGroups() {
+        TJsOddities.LOGGER.info("Registering Item Groups for " + TJsOddities.MOD_ID);
+    }
+    
+}
